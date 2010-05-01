f27fe54463576c2d60725afd9623bfaf362d1af8
Robert Thurnher <r.thurnher@gmail.com>
2010-05-01 22:02:54 +0200

    Mainly some ajaxification.

--- actions.js
+++ actions.js
@@ -4,6 +4,9 @@ include('./model');
 exports.index = function (req) {
     if (req.isPost && req.params.save) {
         new Post({body: req.params.body, created: Date.now()}).save();
+        return skinResponse('skins/posts.html', {
+            allPosts: Post.all().sort(function (a, b) a.created < b.created)
+        });
     }
     return skinResponse('skins/index.html', {
         allPosts: Post.all().sort(function (a, b) a.created < b.created)
